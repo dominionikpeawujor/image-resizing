a765e0ccddbd2549c8ebbf2f49f8fc533f75bce4
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-12-27 13:59:19 +0100

    Added caching

--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -1,26 +1,29 @@
 import express from 'express';
 import { promises as fs } from 'fs';
 import sharp from 'sharp';
+import caching from './caching';
 
 const resize = express.Router();
+resize.use(caching);
 
-
-resize.get('/', (req:express.Request, res:express.Response) => {
+resize.get('/', (req: express.Request, res: express.Response) => {
   try {
-    const filename = req.query.filename
-    const width = req.query.width ;
-    const height = (req.query.height as unknown) as number;
-    console.log(width, typeof(width));
+    const filename = req.query.filename;
+    const width = req.query.width;
+    const height = req.query.height as unknown as number;
 
-    const neww = parseInt(width)
-    const fullPath = `assets/full/${filename}`
+    const neww = parseInt(width);
+    const fullPath = `assets/full/${filename}`;
     const image = sharp(fullPath);
 
     image
       .resize({ width: neww, height: height })
       .toBuffer()
       .then(async (data) => {
-        const makeFile = fs.open(`assets/thumb/${filename}_${width}x${height}.jpg`, 'w+');
+        const makeFile = fs.open(
+          `assets/thumb/${filename}_${width}x${height}.jpg`,
+          'w+'
+        );
         (await makeFile).writeFile(data);
       });
 
