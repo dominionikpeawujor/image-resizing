a85424b7082aaff673e1406e90366cf9d75011d3
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-12-28 19:48:19 +0100

    Added caching

---
+++ build/routes/api/caching.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const path_1 = __importDefault(require("path"));
+const caching = (req, res, next) => {
+    //filename parameters from 'req' object
+    const file = req.query.filename;
+    //File path to get image
+    const filePath = `assets/thumb/${file}_${req.query.width}x${req.query.height}${file.slice(-4)}`;
+    //Conidition for serving cached files (using the existsSync method)
+    const fileExists = (0, fs_1.existsSync)(filePath);
+    try {
+        if (fileExists) {
+            res.sendFile(path_1.default.resolve(filePath));
+        }
+        else {
+            next();
+        }
+    }
+    catch (err) {
+        throw `${err} from caching`;
+    }
+};
+exports.default = caching;
---
+++ src/routes/api/caching.ts
@@ -0,0 +1,32 @@
+import express, { NextFunction } from 'express';
+import { existsSync } from 'fs';
+import path from 'path';
+
+const caching = (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  //filename parameters from 'req' object
+  const file = req.query.filename as unknown as string;
+
+  //File path to get image
+  const filePath = `assets/thumb/${file}_${req.query.width}x${
+    req.query.height
+  }${file.slice(-4)}`;
+
+  //Conidition for serving cached files (using the existsSync method)
+  const fileExists: boolean = existsSync(filePath);
+
+  try {
+    if (fileExists) {
+      res.sendFile(path.resolve(filePath));
+    } else {
+      next();
+    }
+  } catch (err) {
+    throw `${err} from caching`;
+  }
+};
+
+export default caching;
