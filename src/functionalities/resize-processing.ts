e6a78c6eeeb3395ae3a7ef21bffb09245d641f9a
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-12-29 16:36:45 +0100

    Added image processing to a separate module

---
+++ src/functionalities/resize-processing.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+import sharp from 'sharp';
+import { promises as fs } from 'fs';
+
+const processing = async (request: {
+  file: string;
+  width: number;
+  height: number;
+}): Promise<string> => {
+  try {
+    const file = request.file;
+    const width: number = request.width;
+    const height: number = request.height;
+
+    const fullPath = `assets/full/${file}`;
+    const filename = `${file}_${width}x${height}.jpg`;
+    const fileDir = `assets/thumb/${filename}`;
+
+    //Using the sharp module
+    const image = sharp(path.resolve(fullPath));
+
+    //Resizing pictures
+    await image
+      .resize({ width: width, height: height })
+      .toBuffer()
+      .then(async (data) => {
+        const makeFile = fs.open(path.resolve(fileDir), 'w+');
+        (await makeFile).writeFile(data);
+      });
+      
+    return path.resolve(fileDir);
+  } catch (err) {
+    throw `${err}`;
+  }
+};
+
+export default processing;
---
+++ src/middleware/caching.ts
@@ -0,0 +1,42 @@
+import express, { NextFunction } from 'express';
+import { existsSync } from 'fs';
+import path from 'path';
+
+const caching = (
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) => {
+  //filename parameters from 'req' object
+  const file = req.query.filename as unknown as string;
+
+  file === undefined
+    ? res.send('incorrect parameters. Expected "filename" parameter in URL')
+    : '';
+  req.query.width === undefined
+    ? res.send('incorrect parameters. Expected "width" parameter in URL')
+    : '';
+  req.query.height === undefined
+    ? res.send('incorrect parameters. Expected "height" paramete" in URL')
+    : '';
+
+  //File path to get image
+  const filePath = `assets/thumb/${file}_${req.query.width}x${
+    req.query.height
+  }${file.slice(-4)}`;
+
+  //Conidition for serving cached files (using the existsSync method)
+  const fileExists: boolean = existsSync(path.resolve(filePath));
+
+  try {
+    if (fileExists) {
+      res.sendFile(path.resolve(filePath));
+    } else {
+      next();
+    }
+  } catch (err) {
+    throw `${err} from caching`;
+  }
+};
+
+export default caching;
